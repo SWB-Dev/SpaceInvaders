f597a10ac98ab0d6e8413b97144ea4c1a3332887
SWB-Dev <43387066+SWB-Dev@users.noreply.github.com>
2018-11-05 19:58:27 -0700

    Update SpaceInvaders.py

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,10 +1,12 @@
 from turtle import *
 import os
 import math
+import time
 
 playingGame = True
 quitGame = False
 enemies = []
+enemyspeed = 1
 
 def SetupScreen(wn):
 	wn.bgcolor("black")
@@ -35,6 +37,7 @@ class Bullet(Turtle):
 		self.setposition(player.xcor(), player.ycor())
 		self.setheading(90)
 		self.showturtle()
+		self.shapesize(0.5,0.5)
 		self.speed = 10
 		self.collided = False
 		self.moving = True
@@ -47,8 +50,7 @@ class Enemy(Turtle):
 		self.shape("circle")
 		self.penup()
 		self.speed(0)
-		self.setposition(pos,200)
-		self.speed = 10
+		self.setposition(pos,250)
 		self.st()
 
 class Player:
@@ -60,10 +62,11 @@ class Player:
 		self.player.speed(0)
 		self.player.setposition(0,-250)
 		self.player.setheading(90)
-		self.speed = 2
+		self.speed = 15
 		self.moving = False
 		self.direction = ""
 		self.bullets = []
+		self.alive = True
 
 		wn.onkeypress(self.MoveLeft,"Left")
 		wn.onkeyrelease(self.StopMoving,"Left")
@@ -87,7 +90,7 @@ class Player:
 	def Fire(self):
 		bullet = Bullet(self.player)
 		self.bullets.append(bullet)
-		print(len(self.bullets))
+		print(f"Bullets - {len(self.bullets)}")
 
 def CreateEnemies(positions):
 	for pos in positions:
@@ -97,10 +100,13 @@ def MoveBullets(player,enemies):
 	for bullet in player.bullets:
 		bullet.sety(bullet.ycor() + bullet.speed)
 		for enemy in enemies:
-			if IsCollide(enemy,bullet):
+			if IsCollide(bullet,enemy):
 				player.bullets.remove(bullet)
 				bullet.clear()
 				bullet.reset()
+				enemies.remove(enemy)
+				enemy.clear()
+				enemy.reset()
 				continue
 		if bullet.ycor() > 285:
 			player.bullets.remove(bullet)
@@ -109,13 +115,12 @@ def MoveBullets(player,enemies):
 
 
 def IsCollide(t1,t2):
-	distance = math.sqrt(math.pow(t1.xcor() + t2.xcor(),2)) + math.sqrt(math.pow(t1.ycor() + t2.ycor(),2))
-	print(distance)
-	return False
-	# if distance <= 15:
-	# 	return True
-	# else:
-	# 	return False
+	distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(),2)) + math.sqrt(math.pow(t1.ycor() - t2.ycor(),2))
+	if distance < 15:
+		print(distance)
+		return True
+	else:
+		False
 
 def MovePlayer(player):
 	if player.direction == "Left":
@@ -129,21 +134,47 @@ def MovePlayer(player):
 		else:
 			player.player.setx(player.player.xcor() + player.speed)
 
+def MoveEnemies(enemies,player):
+	if not enemies:
+		global playingGame
+		playingGame = False
+		return
+	global enemyspeed
+	moveDown = False
+	for enemy in enemies:
+		enemy.setx(enemy.xcor() + enemyspeed)
+		if IsCollide(player,enemy):
+			Quit()
+		if enemy.xcor() >= 285 or enemy.xcor() <= -285:
+			moveDown = True
+	if moveDown:
+		for enemy in enemies:
+			enemy.sety(enemy.ycor() - 10)
+		enemyspeed *= -1
+
 def Quit():
 	global playingGame
 	playingGame = False
 
-wn = Screen()
-SetupScreen(wn)
-player = Player(wn)
-onkey(Quit,'q')
-CreateEnemies([-200,-100,0,100,200])
+def Main():
+	wn = Screen()
+	wn.delay(0)
+	SetupScreen(wn)
+	player = Player(wn)
+	onkey(Quit,'q')
+	CreateEnemies([-200,-100,0,100,200])
 
-while playingGame:
-	MovePlayer(player)
-	MoveBullets(player,enemies)
+	wn.delay(1)
+
+	while playingGame:
+		delay(0)
+		MovePlayer(player)
+		MoveBullets(player,enemies)
+		MoveEnemies(enemies,player.player)
+		time.sleep(.01)
 
 
-	wn.update()
+	wn.clear()
+	wn.reset()
 
-# mainloop()
+Main()
